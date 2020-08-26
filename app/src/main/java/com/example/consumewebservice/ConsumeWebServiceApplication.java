bc1ede7c60826ae49d15e0877e42a8b36afd048e
David Santacoloma <adsi1407@gmail.com>
2020-08-26 13:29:44 -0500

    Crear clase Application para configuración de Hilt

---
+++ app/src/main/java/com/example/consumewebservice/ConsumeWebServiceApplication.java
@@ -0,0 +1,9 @@
+package com.example.consumewebservice;
+
+import android.app.Application;
+
+import dagger.hilt.android.HiltAndroidApp;
+
+@HiltAndroidApp
+public class ConsumeWebServiceApplication extends Application {
+}
