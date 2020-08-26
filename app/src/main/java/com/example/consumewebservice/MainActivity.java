ad15703178cb1ac346502bdb87f23dc6517fc2e6
David Santacoloma <adsi1407@gmail.com>
2020-08-26 16:44:35 -0500

    Implementar ViewModel en el activity y ajustar inyección de dependencias en el activity

--- app/src/main/java/com/example/consumewebservice/MainActivity.java
+++ app/src/main/java/com/example/consumewebservice/MainActivity.java
@@ -1,14 +1,28 @@
 package com.example.consumewebservice;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.ViewModelProvider;
 
 import android.os.Bundle;
+import android.util.Log;
 
+import com.example.consumewebservice.viewmodel.BrandViewModel;
+
+import dagger.hilt.android.AndroidEntryPoint;
+
+@AndroidEntryPoint
 public class MainActivity extends AppCompatActivity {
+
+    private BrandViewModel brandViewModel;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        brandViewModel = new ViewModelProvider(this).get(BrandViewModel.class);
+        brandViewModel.getAllBrands().observe(this, brands -> {
+            Log.println(Log.VERBOSE, "Brands", "Brands");
+        });
     }
 }
