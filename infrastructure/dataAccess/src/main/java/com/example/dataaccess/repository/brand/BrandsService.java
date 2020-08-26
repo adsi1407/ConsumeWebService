26f67e2f40742b89250ee03a81e102260971a616
David Santacoloma <adsi1407@gmail.com>
2020-08-26 12:57:16 -0500

    Creat DTO e interface para consumir servicio con Retrofit

---
+++ infrastructure/dataAccess/src/main/java/com/example/dataaccess/repository/brand/BrandsService.java
@@ -0,0 +1,14 @@
+package com.example.dataaccess.repository.brand;
+
+import com.example.dataaccess.dto.Brand;
+
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.http.GET;
+
+public interface BrandsService {
+
+    @GET("/carros/marcas")
+    Call<List<Brand>> getAllBrands();
+}
